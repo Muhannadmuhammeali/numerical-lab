8c34e19319d37f47cbcd15d4da6839cb50059904
Muhannad Mohammed Ali <147051331+Muhannadmuhammeali@users.noreply.github.com>
2025-02-05 14:46:27 +0530

    tsp.py

--- tsp.py
+++ tsp.py
@@ -1,23 +1,13 @@
-# Python program to find the shortest possible route
-# that visits every city exactly once and returns to
-# the starting point using memoization and bitmasking
-
 import sys
 def totalCost(mask, pos, n, cost):
   
-    # Base case: if all cities are visited, return the
-    # cost to return to the starting city (0)
     if mask == (1 << n) - 1:
         return cost[pos][0]
 
     ans = sys.maxsize   
-
-    # Try visiting every city that has not been visited yet
     for i in range(n):
         if (mask & (1 << i)) == 0: 
   
-            # If city i is not visited, visit it and 
-             #  update the mask
             ans = min(ans, cost[pos][i] +
                       totalCost(mask | (1 << i), i, n, cost))
 
@@ -27,8 +17,6 @@ def totalCost(mask, pos, n, cost):
 def tsp(cost):
     n = len(cost)
     
-    # Start from city 0, and only city 0 is visited 
-    # initially (mask = 1)
     return totalCost(1, 0, n, cost)
  
 if __name__ == "__main__":
